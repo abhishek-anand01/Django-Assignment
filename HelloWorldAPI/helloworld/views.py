c044712ee36b4b70f601de4160f3f0738d82ac2a
abhishek.anand01 <abhishek.anand01@tigeranalytics.com>
2023-08-31 14:09:13 +0530

    assignment 3

--- HelloWorldAPI/helloworld/views.py
+++ HelloWorldAPI/helloworld/views.py
@@ -4,7 +4,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication  # Import BasicAuthentication
 from django.http import HttpResponse
-
+from rest_framework import generics
+from .models import Person
+from .serializers import PersonSerializer
 class CustomTokenObtainPairView(TokenObtainPairView):
     pass  # No additional modifications needed
 
@@ -15,3 +17,7 @@ class HelloWorldView(APIView):
     def get(self, request):
         message = "Hello World"
         return HttpResponse(message, content_type='text/plain')
+
+class PersonListView(generics.ListAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
